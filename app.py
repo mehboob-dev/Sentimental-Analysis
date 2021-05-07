6cba5581137697488b161c89095e9da972aa9ca5
Meghani M. Mehboob <suitable9004@gmail.com>
2021-05-07 20:41:37 +0530

    Initial Commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from textblob import TextBlob
+
+while True:
+    y = input("Type your Sentence: ")
+    edu = TextBlob(y)
+    x = edu.sentiment.polarity
+    print(x)
+    if x < 0:
+        print("Negative")
+    elif x == 0:
+        print("Neutral")
+    else:
+        print("Positive")
